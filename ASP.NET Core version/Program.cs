924804eebc343e37b3be51ab3467f5d872a33c60
jdouv <44773176+jdouv@users.noreply.github.com>
2019-12-17 21:17:52 +0200

    Minor amendments.

--- ASP.NET Core version/Program.cs
+++ ASP.NET Core version/Program.cs
@@ -7,13 +7,11 @@ namespace Thalassa
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseWindowsService()
                 .UseSystemd()
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .Build().Run();
+        }
     }
 }
